fce85fdc217fdb34378cdc950ff9e30a5366ba4f
Jonathan Reeve <jon.reeve@gmail.com>
2021-02-14 12:26:01 -0500

    first broken attempt to make org-roam reader

---
+++ guide-org/neuron.dhall
@@ -0,0 +1,5 @@
+{ siteTitle = "Test Braindump"
+, siteBaseUrl = Some "https://neuron.zettel.page"
+, editUrl = Some "https://github.com/srid/neuron/edit/master/guide/"
+, formats = [ "markdown", "org" ]
+}
--- neuron/src/app/Neuron/Reader/Org.hs
+++ neuron/src/app/Neuron/Reader/Org.hs
@@ -15,37 +15,102 @@ where
 import qualified Data.Map as Map
 import Data.TagTree (Tag (Tag))
 import Data.Tagged
-import Data.Text (toLower)
+import Data.Text (toLower, isPrefixOf, splitOn)
+import qualified Data.Text.IO as TIO
 import Data.Time.DateMayTime (DateMayTime, parseDateMayTime)
 import Neuron.Reader.Type (ZettelParseError, ZettelReader)
 import Neuron.Zettelkasten.Zettel.Meta (Meta (..))
 import Relude
 import Relude.Extra.Map (lookup)
-import Text.Pandoc (def, runPure)
-import Text.Pandoc.Definition (Pandoc)
+import Text.Pandoc (def, runPure, Format)
+import Text.Pandoc.Definition (Pandoc, Format, Block(RawBlock))
 import Text.Pandoc.Readers.Org (readOrg)
 import Text.Pandoc.Util (getH1)
 
+-- parseOrg :: ZettelReader
+-- parseOrg filePath s = do
+--   doc <- first show $ runPure $ readOrg def s
+--   meta <- extractMetadata doc
+--   pure (meta, doc)
+
+-- -- Extract metadata from the properties that are attached to the first headline
+-- extractMetadata :: Pandoc -> Either ZettelParseError (Maybe Meta)
+-- extractMetadata doc
+--   | Just ((_, _, Map.fromList -> properties), _) <- getH1 doc = do
+--     date <- traverse parseDate $ lookup "date" properties
+--     -- title is now deprecated
+--     let title = Nothing
+--         slug = Nothing
+--         tags = fmap Tag . words <$> lookup "roam_tags" properties
+--         unlisted = parseUnlisted <$> lookup "unlisted" properties
+--     pure $ Just Meta {..}
+--   | otherwise = pure Nothing
+--   where
+--     parseDate :: Text -> Either ZettelParseError DateMayTime
+--     parseDate date = maybeToRight (Tagged $ "Invalid date format: " <> date) $ parseDateMayTime @Maybe date
+--     parseUnlisted :: Text -> Bool
+--     parseUnlisted a = toLower a == "true"
+
+-- extractMetadata filePath doc = case doc of
+--   Left -> Nothing
+--   Right (Pandoc meta blocks)-> Meta {title = title,
+--                                      slug = slug,
+--                                      tags = tags,
+--                                      date = date} where
+--     title = lookupMeta "title" meta
+--     slug = parseSlug filePath
+--     date = parseDate filePath
+--     tags = parseTags doc
+
+
+-- parseDate :: FilePath -> Either ZettelParseError DateMayTime
+-- parseDate filePath = ...
+
+
 parseOrg :: ZettelReader
-parseOrg _ s = do
+parseOrg filePath s = do
   doc <- first show $ runPure $ readOrg def s
-  meta <- extractMetadata doc
+  meta <- extractMetadata doc filePath
   pure (meta, doc)
 
--- | Extract metadata from the properties that are attached to the first headline
 extractMetadata :: Pandoc -> Either ZettelParseError (Maybe Meta)
-extractMetadata doc
-  | Just ((_, _, Map.fromList -> properties), _) <- getH1 doc = do
-    date <- traverse parseDate $ lookup "date" properties
-    -- title is now deprecated
-    let title = Nothing
-        slug = Nothing
-        tags = fmap Tag . words <$> lookup "tags" properties
-        unlisted = parseUnlisted <$> lookup "unlisted" properties
+extractMetadata doc filePath = case doc of
+  Meta meta [Block block] -> do
+    let title = parseTitle meta
+        date = parseDate filePath
+        slug = parseSlug filePath
+        tags = parseTags blocks
+        -- TODO?
+        unlisted = False
     pure $ Just Meta {..}
-  | otherwise = pure Nothing
-  where
-    parseDate :: Text -> Either ZettelParseError DateMayTime
-    parseDate date = maybeToRight (Tagged $ "Invalid date format: " <> date) $ parseDateMayTime @Maybe date
-    parseUnlisted :: Text -> Bool
-    parseUnlisted a = toLower a == "true"
+  _ -> pure Nothing
+
+-- Org-roam dates are stored in the filename.
+-- They have the form: 20210102133406 which is 2021-01-02, 13:34:06
+parseDate :: FilePath -> Maybe DateMayTime
+parseDate fp = case splitOn "-" fp of
+  date : slug | length date == 14 -> fp -- TODO
+  _ -> Nothing
+
+parseSlug fp :: FilePath -> Slug
+parseSlug fp = fp -- TODO
+
+-- | Tags in org-roam look like: @#+roam_tags: tag1 tag2 "tag three"@
+-- I.e., they're space-delimited, and can contain spaces if quoted.
+-- These aren't really parsed by Pandoc, but appear as Blocks:
+-- > RawBlock (Format "org") "#+roam_tags: tag1 tag2 "tag three"
+parseTags :: [Block] -> Maybe [Tag]
+parseTags blocks =  viaNonEmpty head $ mapMaybe blockToTag blocks where
+  blockToTag :: Block -> Maybe [Tag]
+  blockToTag block = case block of
+    RawBlock fmt blockText | fmt == "org"
+      && Data.Text.isPrefixOf "#+roam_tags" (toLower blockText)
+      && length (words blockText) < 2 -> Just $ map Tag (words blockText)
+    _ -> Nothing
+
+-- testReader :: IO ()
+testReader = do
+  let fp = "/home/jon/Code/neuron/guide-org/test.org" :: FilePath
+  testFile <- TIO.readFile fp
+  let result = parseOrg fp testFile
+  return result
